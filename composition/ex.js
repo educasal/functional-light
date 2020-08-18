0c231fb103fe7a58d97819eecbc73c6cafc63a26
Eduardo Fernandez <eduardo.fernandez@clickdimensions.com>
2020-08-18 21:32:06 +0200

    Exercise about composition

---
+++ composition/ex.js
@@ -0,0 +1,34 @@
+"use strict";
+
+function increment(x) { return x + 1; }
+function decrement(x) { return x - 1; }
+function double(x) { return x * 2; }
+function half(x) { return x / 2; }
+
+function compose(...funcs) {
+  const reversed = funcs.reverse();
+  return pipe(...reversed);
+}
+
+function pipe(...funcs) {
+  return function piped(arg) {
+    for (const func of funcs) {
+      arg = func(arg);
+    }
+    return arg;
+  }
+}
+
+var f1 = compose(increment,decrement);
+var f2 = pipe(decrement,increment);
+var f3 = compose(decrement,double,increment,half);
+var f4 = pipe(half,increment,double,decrement);
+var f5 = compose(increment);
+var f6 = pipe(increment);
+
+console.log( f1(3) === 3 );
+console.log( f1(3) === f2(3) );
+console.log( f3(3) === 4 );
+console.log( f3(3) === f4(3) );
+console.log( f5(3) === 4 );
+console.log( f5(3) === f6(3) );
